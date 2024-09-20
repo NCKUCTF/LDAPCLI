1d5db9d1383845c147e6700aee689bcedbf045db
Chummy <jimmy012403976@gmail.com>
2024-09-20 19:09:51 +0800

    Fix install

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ then
 		set -e
 		sudo apt-get install -y curl wget ldap-utils libarchive-zip-perl jq
 		arch=$(dpkg --print-architecture)
-		wget https://github.com/mikefarah/yq/releases/download/v4.17.2/yq_linux_${arch}.tar.gz -O - | tar xz && sudo mv yq_linux_${arch} /usr/local/bin/yq
+		wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_${arch}.tar.gz -O - | tar xz && sudo mv yq_linux_${arch} /usr/local/bin/yq
 	else
 		echo "os not support"
 		exit 0
@@ -41,8 +41,8 @@ fi
 
 for a in $(ls | grep -P "^ldap.*\.sh" | grep -v "$(echo "$0" | sed "s/^.*\///g")")
 do
-	cp $a /usr/local/bin/$(echo "$a" | sed "s/\.sh//g")
-	chmod +x /usr/local/bin/$(echo "$a" | sed "s/\.sh//g")
+	chmod +x $a
+    ln -s $(pwd)/$a /usr/local/bin/$(echo "$a" | sed "s/\.sh//g")
 done 
 
 echo ""
